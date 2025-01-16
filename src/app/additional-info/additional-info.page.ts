83fecc1efbbe87cf67b971ea2949dd59a6a61bf3
KozielGPC <gpcgabriel0@gmail.com>
2025-01-15 22:52:29 -0300

    feat: add database query

--- src/app/additional-info/additional-info.page.ts
+++ src/app/additional-info/additional-info.page.ts
@@ -14,27 +14,22 @@ export class AdditionalInfoPage implements OnInit {
   weight: number | null = null;
   medicationStartDate: string = '';
 
-  userId: string = '123';
-
   constructor(private _router: Router, private userService: UserService) {}
 
   ngOnInit() {}
 
   async submit() {
     const userData = {
-      full_name: this.fullName,
+      name: this.fullName,
       age: this.age,
-      gender: this.gender,
+      sex: this.gender,
       height: this.height,
       weight: this.weight,
-      medication_start_date: this.medicationStartDate,
+      medication_started_at: this.medicationStartDate,
     };
 
     try {
-      const updatedUser = await this.userService.updateUser(
-        this.userId,
-        userData
-      );
+      const updatedUser = await this.userService.updateUser(userData);
       console.log('User updated:', updatedUser);
       this._router.navigate(['/home']);
     } catch (error) {
--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -8,15 +8,52 @@ import { SupabaseClient } from '@supabase/supabase-js';
 export class UserService {
   constructor(private _supabase: SupabaseClient, private _router: Router) {}
 
-  async updateUser(userId: string, userData: any): Promise<any> {
+  async updateUser(userData: any): Promise<any> {
     try {
-      const { data, error } = await this._supabase
+      const {
+        data: { user: supabaseUser },
+        error: supabaseUserError,
+      } = await this._supabase.auth.getUser();
+
+      if (supabaseUserError) {
+        console.error('Error fetching user from supabase:', supabaseUserError);
+        throw supabaseUserError;
+      }
+
+      const { data: user, error: userError } = await this._supabase
         .from('users')
-        .update(userData)
-        .eq('id', userId);
+        .select()
+        .eq('email', supabaseUser?.email)
+        .single();
+
+      if (userError) {
+        console.error('Error fetching user from supabase:', userError);
+        throw userError;
+      }
+
+      if (!user) {
+        throw new Error('User not found with email: ' + supabaseUser?.email);
+      }
+
+      const { data, error } = await this._supabase
+        .from('profiles')
+        .upsert({
+          id: user.id,
+          ...userData,
+        })
+        .select()
+        .single();
 
       if (error) {
         throw error;
+      }
+
+      const { error: updateError } = await this._supabase
+        .from('user_profiles')
+        .upsert({ user_id: user.id, profile_id: data.id, scope: 'user' });
+
+      if (updateError) {
+        throw updateError;
       }
 
       return data;
